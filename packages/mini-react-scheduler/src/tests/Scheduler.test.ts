da3fc8430adad98fd2aadc67f8b5cb893eb7f1a6
gamejoye <gamejoye@gmail.com>
2024-06-13 02:38:34 +0800

    tests(scheduler): test scheduler

---
+++ packages/mini-react-scheduler/src/tests/Scheduler.test.ts
@@ -0,0 +1,57 @@
+import { scheduleCallback } from "../Scheduler";
+
+describe('Scheduler', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  function sleep(time: number) {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    })
+  }
+
+  function createJestFnWithReturnTimes(maxCallbackReturns: number) {
+    let currentCallbackReturns = 0;
+    const callback = jest.fn(() => {
+      currentCallbackReturns++;
+      if (currentCallbackReturns <= maxCallbackReturns) {
+        return callback;
+      }
+    })
+    return callback;
+  }
+
+  function createJestFnWithLongRunning(initBlockTime: number, descPerReturn: number) {
+    let blockTime = initBlockTime;
+    const callback = jest.fn(() => {
+      if (blockTime >= 0) {
+        const now = (new Date()).getTime();
+        while ((new Date()).getTime() - now < blockTime);
+        blockTime -= descPerReturn;
+      }
+      if (blockTime >= 0) {
+        return callback;
+      }
+    })
+    return callback;
+  }
+
+  it('scheduleCallback', async () => {
+    const firstCallback = createJestFnWithReturnTimes(4);
+    const secondCallback = createJestFnWithReturnTimes(9);
+    scheduleCallback(firstCallback);
+    scheduleCallback(secondCallback);
+    await sleep(500);
+    expect(firstCallback).toHaveBeenCalledTimes(5);
+    expect(secondCallback).toHaveBeenCalledTimes(10);
+
+    const longTimeCallback = createJestFnWithLongRunning(1000, 600);
+    // 1000 + 400
+    scheduleCallback(longTimeCallback);
+    await sleep(1500);
+    expect(longTimeCallback).toHaveBeenCalledTimes(2);
+  }, 10000);
+});
